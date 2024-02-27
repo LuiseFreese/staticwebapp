3ae54c9f8b626fc9419a8173713f3e981d592bdb
Luise Freese <luise@raeuberleiterin.de>
2024-02-27 11:52:49 +0100

    recipe

--- vue-app/src/views/about.vue
+++ vue-app/src/views/about.vue
@@ -3,12 +3,12 @@
     <div class="content-title-group not-found">
       <h2 class="title">How to make Ginger Shots</h2>
       <p>
-        `Mix all ingredients in a blender, when smooth, strain through a cheesecloth and fill into small bottles.`
+        ``Mix all ingredients in a blender, when smooth, strain through a cheesecloth and fill into small bottles.``
       </p>
       <br />
       <h2 class="title">Enjoy</h2>
       <p>
-        `Drink one shot each day :-)`
+        ``Drink one shot each day :-)``
       </p>
     </div>
   </div>
